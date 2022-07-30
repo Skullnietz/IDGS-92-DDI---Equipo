5c025fdb3396b50367f58dba26572869a85dad16
Eber Palma <al221911577@gmail.com>
2022-07-29 21:01:01 -0500

    Correccion boton producto

--- src/navigator/ScreenStack/catalogo/ProductoComponent.js
+++ src/navigator/ScreenStack/catalogo/ProductoComponent.js
@@ -2,85 +2,112 @@ import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
-    Box,
-    AspectRatio,
-    Image,
-    Stack,
-    Text,
-    NativeBaseProvider,
-    Button,
-    Center,
-    HStack,
-    Flex,
-    Heading,
-    Link,
-  } from 'native-base';
+  Box,
+  AspectRatio,
+  Image,
+  Stack,
+  Text,
+  NativeBaseProvider,
+  Button,
+  Center,
+  HStack,
+  Flex,
+  Heading,
+  Link,
+} from 'native-base';
 
-export default function ProductoComponent(props){
-    function ButtonSelector(){
-        if( props.categoria == 'PRODUCTOS'){
-            return (
-                <Button
-                bgColor={'black'}
-                style={{position: 'relative', right:'-40%'}}
-                ><Flex direction="row"><Text color={"white"} style={{fontSize: 15}} justifyContent={'center'}>Solicitar</Text> <Icon name ="logo-whatsapp" size={25} color="white" style={{ right:'-25%'}} /></Flex>
-                    
-                    
-                </Button>
-            );
-        }
-        if( props.categoria == 'SERVICIOS'){
-            return (
-                <Link w={'15%'} 
-                    direction="row" 
-                    href="https://api.whatsapp.com/send?phone=+527228974086&text=Hola"
-                >
-                    <Icon name ="logo-whatsapp" size={25} color="green" style={{position: 'relative', right:'-150%'}}/>
-                </Link>
-            );
-        }
+export default function ProductoComponent(props) {
+  function ButtonSelector() {
+    if (props.categoria == 'PRODUCTOS') {
+      return (
+        <Button bgColor={'black'} style={{position: 'relative', right: '-5%'}}>
+          <Flex direction="row">
+            <Text
+              color={'white'}
+              style={{fontSize: 15}}
+              justifyContent={'center'}>
+              Solicitar
+            </Text>{' '}
+            <Icon
+              name="logo-whatsapp"
+              size={25}
+              color="white"
+              style={{right: '-25%'}}
+            />
+          </Flex>
+        </Button>
+      );
     }
+    if (props.categoria == 'SERVICIOS') {
+      return (
+        <Link
+          w={'15%'}
+          direction="row"
+          href="https://api.whatsapp.com/send?phone=+527228974086&text=Hola">
+          <Icon
+            name="logo-whatsapp"
+            size={25}
+            color="green"
+            style={{position: 'relative', right: '-150%'}}
+          />
+        </Link>
+      );
+    }
+  }
 
-    return (
-        
-            <Box m={2} maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-                    borderColor: "coolGray.600",
-                    backgroundColor: "gray.700"
-                    }} _web={{
-                    shadow: 2,
-                    borderWidth: 0
-                    }} _light={{
-                    backgroundColor: "gray.50"
-                    }}>
-                        <Box>
-                        <AspectRatio w="100%" ratio={1/1}>
-                            <Image source={{
-                            uri: props.image
-                        }} alt="image" />
-                        </AspectRatio>
-                        <Center bg= {props.categoria == "SERVICIOS" ? "violet.400": 'gray.500'}  _dark={{
-                        bg: "violet.400"
-                        }} _text={{
-                        color: "warmGray.50",
-                        fontWeight: "700",
-                        fontSize: "xs"
-                        }} position="absolute" bottom="0" px="3" py="1.5">
-                            {props.categoria}
-                        </Center>
-                        </Box>
-                        <Flex p="4" direction='row'>
-                        <Stack space={2}>
-                            <Heading size="md" ml="-1">
-                            {props.name}
-                            </Heading>
-                        </Stack>
-                        <Flex direction="row" justifyContent={'center'}>
-                            
+  return (
+    <Box
+      m={2}
+      maxW="80"
+      rounded="lg"
+      overflow="hidden"
+      borderColor="coolGray.200"
+      borderWidth="1"
+      _dark={{
+        borderColor: 'coolGray.600',
+        backgroundColor: 'gray.700',
+      }}
+      _web={{
+        shadow: 2,
+        borderWidth: 0,
+      }}
+      _light={{
+        backgroundColor: 'gray.50',
+      }}>
+      <Box>
+        <AspectRatio w="100%" ratio={1 / 1}>
+          <Image
+            source={{
+              uri: props.image,
+            }}
+            alt="image"
+          />
+        </AspectRatio>
+        <Center
+          bg={props.categoria == 'SERVICIOS' ? 'violet.400' : 'gray.500'}
+          _dark={{
+            bg: 'violet.400',
+          }}
+          _text={{
+            color: 'warmGray.50',
+            fontWeight: '700',
+            fontSize: 'xs',
+          }}
+          position="absolute"
+          bottom="0"
+          px="3"
+          py="1.5">
+          {props.categoria}
+        </Center>
+      </Box>
 
-                            <ButtonSelector></ButtonSelector>
-                        </Flex>
-                        </Flex>
-                    </Box> 
-        
-    )
+      <Flex direction="row">
+        <Box w={'60%'}>
+          <Heading ml={1.5}>{props.name}</Heading>
+        </Box>
+
+        <ButtonSelector></ButtonSelector>
+      </Flex>
+    </Box>
+  );
 }
